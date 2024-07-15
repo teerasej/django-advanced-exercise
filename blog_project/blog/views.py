381ae60c431c9a4edb13c381f045044fcb39334e
Teerasej <teerasej@gmail.com>
2024-07-15 12:27:10 +0700

    feat: Add viewsets for Author and Post models
    This commit adds viewsets for the Author and Post models in the blog application. The AuthorViewSet and PostViewSet classes are created to handle the CRUD operations for Author and Post objects respectively. The viewsets are configured with the appropriate queryset and serializer_class to enable the API endpoints for these models.

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+# blog/views.py
+from rest_framework import viewsets
+from .models import Author, Post
+from .serializers import AuthorSerializer, PostSerializer
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
