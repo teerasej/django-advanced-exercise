45a22ae8ba72e082f5d46cd799632a1ac6d22815
Teerasej <teerasej@gmail.com>
2024-07-16 19:49:53 +0700

    add function to trigger by url request

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -1,7 +1,7 @@
 # blog/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AuthorViewSet, PostViewSet
+from .views import AuthorViewSet, PostViewSet, trigger_error
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -15,4 +15,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('sentry-debug/', trigger_error),
 ]
